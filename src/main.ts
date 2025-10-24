04084952aa9d68eee265f5ca2bc2cdf938273256
Zulkefal <zulkefal.mohammadzai705@gmail.com>
2025-10-24 13:01:38 +0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverless from 'serverless-http';
+
+let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.setGlobalPrefix('api');
 
   app.enableCors({
@@ -11,7 +13,15 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT || 5000;
-  await app.listen(port);
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverless(expressApp);
 }
-bootstrap();
+
+// 👇 Vercel expects this default export
+export default async function handler(req, res) {
+  if (!server) {
+    server = await bootstrap();
+  }
+  return server(req, res);
+}
